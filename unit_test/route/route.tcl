7dad7f07a5f58290f205bb4bc1a1d621f49a8874
Juexiao Su <juexiao.su@gmail.com>
2017-12-18 13:35:35 -0800

    JSU: 1) add unit test for routing graph

---
+++ unit_test/route/route.tcl
@@ -0,0 +1,36 @@
+#Author: Juexiao Su
+#Purpose: Test basic functionality
+
+#puts "#########################################"
+#puts "#        read blif netlist              #"
+#puts "#########################################"
+set design 3gate.blif
+read_blif $design
+gen_dwave_nl
+puts "\n"
+
+puts "#########################################"
+puts "#     initialize hardware target        #"
+puts "#########################################"
+init_target -row 2 -col 2 -local 8
+#write_blif 3gate_dwave.blif
+puts "\n"
+
+puts "#########################################"
+puts "#     initialize place and route        #"
+puts "#########################################"
+init_system 
+puts "\n"
+
+#puts "#########################################"
+#puts "#           place netlist               #"
+#puts "#########################################"
+#place
+
+
+puts "#########################################"
+puts "#     check routing graph               #"
+puts "#########################################"
+check_routing_graph
+puts "\n"
+
